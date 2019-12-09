7d5287f266f2a5a2f911be5f7e2daa938b0bbf2f
Alex Engelhart <asengelhart@yahoo.com>
2019-12-09 04:48:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,12 +4,14 @@ require 'yaml'
 def load_library(path)
   basehash = YAML.load_file(path)
   result = {}
-  result['get_meaning'] = basehash.reduce({}) do |memo, (meaning, emoticons)|
+  result[:get_meaning] = basehash.reduce({}) do |memo, (meaning, emoticons)|
     jp_emoticon = emoticons[1]
     memo[jp_emoticon] = meaning 
     memo 
   end 
-  result['get_emoticon']
+  result[:get_emoticon] = basehash.reduce({}) do |memo, (meaning, emoticons)|
+    
+  end 
   
 end
 
